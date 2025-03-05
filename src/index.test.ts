f4cd62eca3d9c305e0fb2716443e410489ea823d
woodytamil <selvantamil381@gmail.com>
2025-03-05 07:41:27 +0530

    CI

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("Run this test",()=>{
+    it("should pass CI", ()=>{
+        expect(1).toBe(1)
+    })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export const hello = "hello"
+console.log("he")
